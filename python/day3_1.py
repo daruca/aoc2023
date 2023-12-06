7db2840ade6fe6dd7919ec44ffaf0ae96ff04903
daruca <davidrubioc@gmail.com>
2023-12-06 22:13:58 +0100

    day3 start

--- python/day3_1.py
+++ python/day3_1.py
@@ -1,2 +1,8 @@
-f = open("input3.txt","r")
+f = open("input3_tst.txt","r")
 t = f.readlines()
+matrix = []
+for line in t:
+    values = line.split(".")
+    matrix.append(values)
+    
+print(matrix)
